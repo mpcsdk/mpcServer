c769db2f070fa5658a30fcbf7ca69ce79278f4af
frank <no>
2023-08-04 18:34:12 +0800

    randrpivatekey

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module li17server
 go 1.15
 
 require (
+	github.com/ethereum/go-ethereum v1.12.0
 	github.com/gogf/gf/contrib/nosql/redis/v2 v2.5.1
 	github.com/gogf/gf/v2 v2.5.1
 	github.com/panjf2000/ants/v2 v2.8.1
--- internal/controller/sign/sign_new.go
+++ internal/controller/sign/sign_new.go
@@ -1,6 +1,11 @@
 package sign
 
-import "li17server/api/sign"
+import (
+	"li17server/api/sign"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 type ControllerV1 struct{}
 
@@ -8,4 +13,11 @@ var tmp_privkey2 string = "0ac7d64995c6b4daac2688c0e40d25af50887ada5b7a4cbe197ad
 
 func NewV1() sign.ISignV1 {
 	return &ControllerV1{}
+}
+
+func randPrivateKey() string {
+	pk, _ := crypto.GenerateKey()
+	pkBytes := crypto.FromECDSA(pk)
+	pkstr := hexutil.Encode(pkBytes)[2:]
+	return pkstr
 }
