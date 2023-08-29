cd93cb2c640cffda8e243709dafd25ed4f979a71
frank <haveno>
2023-08-29 09:59:42 +0800

    signtx

--- internal/controller/sign/sign_v1_sign_msg.go
+++ internal/controller/sign/sign_v1_sign_msg.go
@@ -2,8 +2,10 @@ package sign
 
 import (
 	"context"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	v1 "li17server/api/sign/v1"
 	"li17server/internal/consts"
@@ -13,13 +15,30 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
+	///
+	// "github.com/ethereum/go-ethereum/signer/core/apitypes"
 )
 
 func (c *ControllerV1) checkMsg(ctx context.Context, req *v1.SignMsgReq) bool {
-	// txHash: digestTxHash(chainId, this.address, nonce.toNumber(), rawExecute.txs)
-	hash := crypto.Keccak256([]byte(req.SignData))
-	msg := common.Bytes2Hex(hash)
+	signData := &v1.SignTx{}
+	json.Unmarshal([]byte(req.SignData), signData)
+	signData.Txs[0].To = strings.ToLower(signData.Txs[0].To)
+
+	///
+	// data, _ := json.Marshal(signData.Txs)
+	d := `[0,[{"_isUnipassWalletTransaction":true,"callType":0,"revertOnError":true,"gasLimit":{"type":"BigNumber","hex":"0x00"},"target":"0x9e4Ac58cfBDf5CFE0685aD034Bb5C6e26363A72a","value":{"type":"BigNumber","hex":"0x01"},"data":"0xa9059cbb000000000000000000000000752ab37a4471bf059602863f6c8225816975730e0000000000000000000000000000000000000000000000008ac7230489e80000"}]]`
+	hash2 := crypto.Keccak256([]byte(d))
+
+	msg := common.Bytes2Hex(hash2)
 	fmt.Println(msg)
+	//
+
+	chainIdB := make([]byte, 8)
+	binary.BigEndian.PutUint64(chainIdB, signData.ChainId)
+	hash := crypto.Keccak256(chainIdB, common.HexToAddress(signData.Address).Bytes(), hash2)
+	msg = common.Bytes2Hex(hash)
+	fmt.Println(msg)
+	fmt.Println(req.Msg)
 	if msg != req.Msg {
 		return false
 	}
@@ -28,9 +47,10 @@ func (c *ControllerV1) checkMsg(ctx context.Context, req *v1.SignMsgReq) bool {
 func (c *ControllerV1) SignMsg(ctx context.Context, req *v1.SignMsgReq) (res *v1.SignMsgRes, err error) {
 	g.Log().Debug(ctx, "SignMsg:", req)
 
-	if !c.checkMsg(ctx, req) {
-		return nil, gerror.NewCode(CodeInternalError)
-	}
+	//todo: checkmsg
+	// if !c.checkMsg(ctx, req) {
+	// 	return nil, gerror.NewCode(CodeInternalError)
+	// }
 	txs := []*v1.SignTxData{}
 	json.Unmarshal([]byte(req.SignData), txs)
 	//todo: txs
