2dc34577eaa0e4060ad97feebe12e5962bcfb707
frank <no>
2023-10-16 18:39:38 +0800

    cpuCore

--- internal/logic/generator/new.go
+++ internal/logic/generator/new.go
@@ -5,6 +5,8 @@ import (
 	"li17server/internal/service"
 	"time"
 
+	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/panjf2000/ants/v2"
 )
 
@@ -14,7 +16,7 @@ type sGenerator struct {
 }
 
 func New() *sGenerator {
-	p, _ := ants.NewPool(10)
+	p, _ := ants.NewPool(core)
 
 	return &sGenerator{
 		ctx:  context.Background(),
@@ -23,8 +25,16 @@ func New() *sGenerator {
 }
 
 var sessionDur time.Duration = 0
+var core = 2
 
 func init() {
+	ctx := gctx.GetInitCtx()
+	cfg := gcfg.Instance()
+	v, err := cfg.Get(ctx, "server.cpuCore")
+	if err != nil {
+		panic(err)
+	}
+	core = v.Int()
 	service.RegisterGenerator(New())
 
 }
